d33c3421939126026216574247906dfb5daae827
Deatdreicer <jeferson91pineros@gmail.com>
2020-11-25 21:00:49 -0500

    Inyeccion vista

---
+++ miscarritos/resources/views/Nosotros.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE HTML>
+<!--
+	Halcyonic by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+    <title>Nosotros</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body class="subpage">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <section id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Logo -->
+                        <h1><a href="index.html" id="logo">Mis Carritos.com</a></h1>
+
+                        <!-- Nav -->
+                        <nav id="nav">
+                            <a href="index.html">Home</a>
+                            <a href="Servicios.html">Servicios</a>
+                            <a href="#">Two Column #1</a>
+                            <a href="#">Two Column #2</a>
+                            <a href="#">Contactenos</a>
+                        </nav>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content -->
+        <section id="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Main Content -->
+                        <section>
+                            <header>
+                                <h2>Quienes Somos</h2>
+                            </header>
+                            <p>
+                                Mis Carritos.com ofrece una opción creada para movilizarse dentro y fuera de la ciudad con tus amigos o familiares, también ofrecemos servicios empresariales, estamos comprometidos con la excelencia en el servicio encontraras claridad en tarifas y políticas.
+                                ¡Has llegado al lugar correcto! Revalorizamos tu tiempo para que puedas continuar finiquitando los demás detalles del viaje, obtienes tranquilidad y seguridad con reservas.
+                            </p>
+                        </section>
+
+                        <section>
+                            <header>
+                                <h2>Misión</h2>
+                            </header>
+                            <p>
+                                Prestar el mejor servicio de transporte, con el más alto nivel ético y profesional, procurando siempre satisfacer las necesidades de nuestros clientes, contribuyendo para que su desplazamiento sea lo más cómodo posible.
+                            </p>
+                        </section>
+
+                        <section>
+                            <header>
+                                <h2>Visión</h2>
+                            </header>
+                            <p>
+                                Servir eficientemente, siendo fiel a nuestros principios y valores corporativos, generando bienestar, seguridad y confianza en nuestros clientes, usuarios, Empleados y todo aquel que solicite nuestros Servicios de Transporte Especial, Alquiler de Vehículos,
+                                rastreo satelital, Consultoría y de Capacitación. Satisfaciendo eficazmente, y con entusiasmo, la necesidad del servicio que ha solicitado.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <section id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 col-12-medium">
+
+                        <!-- Links -->
+                        <section>
+                            <h2>Links to Important Stuff</h2>
+                            <div>
+                                <div class="row">
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="#">Neque amet dapibus</a></li>
+                                            <li><a href="#">Sed mattis quis rutrum</a></li>
+                                            <li><a href="#">Accumsan suspendisse</a></li>
+                                            <li><a href="#">Eu varius vitae magna</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="#">Neque amet dapibus</a></li>
+                                            <li><a href="#">Sed mattis quis rutrum</a></li>
+                                            <li><a href="#">Accumsan suspendisse</a></li>
+                                            <li><a href="#">Eu varius vitae magna</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="#">Neque amet dapibus</a></li>
+                                            <li><a href="#">Sed mattis quis rutrum</a></li>
+                                            <li><a href="#">Accumsan suspendisse</a></li>
+                                            <li><a href="#">Eu varius vitae magna</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="#">Neque amet dapibus</a></li>
+                                            <li><a href="#">Sed mattis quis rutrum</a></li>
+                                            <li><a href="#">Accumsan suspendisse</a></li>
+                                            <li><a href="#">Eu varius vitae magna</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                    <div class="col-4 col-12-medium imp-medium">
+
+                        <!-- Blurb -->
+                        <section>
+                            <h2>An Informative Text Blurb</h2>
+                            <p>
+                                Duis neque nisi, dapibus sed mattis quis, rutrum accumsan sed. Suspendisse eu varius nibh. Suspendisse vitae magna eget odio amet mollis. Duis neque nisi, dapibus sed mattis quis, sed rutrum accumsan sed. Suspendisse eu varius nibh lorem ipsum amet dolor
+                                sit amet lorem ipsum consequat gravida justo mollis.
+                            </p>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Copyright -->
+        <div id="copyright">
+            &copy; Untitled. All rights reserved. | Design: <a href="http://html5up.net">HTML5 UP</a>
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
