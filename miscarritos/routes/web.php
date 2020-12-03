3a967a07c97d3101287f312e213770d748ae82e3
Deatdreicer <jeferson91pineros@gmail.com>
2020-12-02 20:11:01 -0500

    fix: cambios

--- miscarritos/routes/web.php
+++ miscarritos/routes/web.php
@@ -30,5 +30,5 @@ Route::get('nosotros', [MiscarritosController::class, 'nosotros'])->name('nosotr
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
