7df10a49ce2341924aff2ff6a6d925cf8c74b372
Deatdreicer <jeferson91pineros@gmail.com>
2020-12-12 16:51:17 -0500

    fix: integración entidades

--- miscarritos/app/Http/Controllers/HomeController.php
+++ miscarritos/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,12 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function pruebaConsulta() {
+        $usuario = DB::select('SELECT * FROM users', [1]);
+        foreach($usuario as $user){
+            echo $user->nombre;
+        }
     }
 }
--- miscarritos/routes/web.php
+++ miscarritos/routes/web.php
@@ -32,3 +32,4 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+
