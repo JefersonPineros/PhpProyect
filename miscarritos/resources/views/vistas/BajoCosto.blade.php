8f6e222f580a069ab86f5fcd0b618b8244d77af6
rubyromero8404 <rubyromero8404@gmail.com>
2020-12-13 18:08:27 -0500

    nvas vistas

--- miscarritos/resources/views/vistas/BajoCosto.blade.php
+++ miscarritos/resources/views/vistas/BajoCosto.blade.php
@@ -1,43 +1,16 @@
-<!DOCTYPE HTML>
+@extends('layout.Platilla')
 
-<html>
-
-<head>
-    <title>BajoCosto</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-</head>
-
-<body>
-    <div id="page-wrapper">
+@section('css')
+    <link rel="stylesheet" href="static/assets/css/tarjetas.css">
+@endsection
+@section('presentacion')
 
         <!-- Header -->
         <section id="header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-
-                        <!-- Logo -->
-                        <h1><a href="index.html" id="logo">Mis Carritos.com</a></h1>
-
-                        <!-- Nav -->
-                        <nav id="nav">
-                            <a href="index.html">Home</a>
-                            <a href="Nosotros.html">Nosotros</a>
-                            <a href="Servicios.html">Servicios</a>
-                            <a href="InfoAdicional.html">Información Adicional</a>
-                            <a href="">Contactenos</a>
-                        </nav>
-
-                    </div>
-                </div>
-            </div>
             <div id="banner">
                 <div class="container">
                     <div class="row">
                         <div class="col-6 col-12-medium">
-
                             <!-- Banner Copy -->
                             <p>Vehículos Bajo Costo</p>
                         </div>
@@ -323,79 +296,4 @@
                 </div>
         </section>
 
-        <!-- Footer -->
-        <section id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-8 col-12-medium">
-
-                        <!-- Links -->
-                        <section>
-                            <h2>Siguenos</h2>
-                            <div>
-                                <div class="row">
-                                    <div class="col-4 col-12-medium imp-medium">
-                                        <ul class="link-list last-child">
-                                            <li><a href="#">Facebook</a></li>
-                                            <br>
-                                            <li><a href="#">Twitter</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-4 col-12-medium imp-medium">
-                                        <ul class="link-list last-child">
-                                            <li><a href="#">Instagram</a></li>
-                                            <br>
-                                            <li><a href="#">LinkedIn</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-
-                    </div>
-                    <div class="col-4 col-12-medium imp-medium">
-
-                        <!-- Blurb -->
-                        <section>
-                            <h2>Encuentranos</h2>
-                            <div>
-                                <strong>
-									Direccion:
-									<br>
-								</strong>Carrera 13 N° 65-10 Barrio Chapinero - Bogotá, Colombia.
-                            </div>
-                            <div>
-                                <strong>PBX:</strong>
-                                <a>(1) 5461600</a>
-                                <br>
-                                <strong>Móvil</strong>
-                                <a>+57 (318) 6625068</a>
-                            </div>
-                            <div>
-                                <strong>Email:</strong>
-                                <a>skillex.gaes@gmail.com</a>
-                            </div>
-                        </section>
-
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <!-- Copyright -->
-        <div id="copyright">
-            &copy; Skillex. All rights reserved. | Design: Raven Design
-        </div>
-
-        </div>
-
-        <!-- Scripts -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/browser.min.js"></script>
-        <script src="assets/js/breakpoints.min.js"></script>
-        <script src="assets/js/util.js"></script>
-        <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+        @endsection
