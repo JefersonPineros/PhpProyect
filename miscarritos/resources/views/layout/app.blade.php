370f8c47bef6eeded0ee3213a418b55a418e0992
GohanSebas <gohansebas_94@hotmail.com>
2020-12-14 18:21:00 -0500

    Cambios reset pass

--- miscarritos/resources/views/layout/app.blade.php
+++ miscarritos/resources/views/layout/app.blade.php
@@ -49,7 +49,7 @@
                             
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
                                 </li>
                             @endif
                         @else
